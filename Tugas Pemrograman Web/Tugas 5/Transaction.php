e4f831ec6d7ddd52613eee1985f29f04fc246159
Hanzalahnur Solikhin <hanzalahnursolikhin@gmail.com>
2026-09-22 10:58:25 +0800

    Buat folder dan file Tugas 5

---
+++ Tugas Pemrograman Web/Tugas 5/Transaction.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+class Transaction
+{
+    public function __construct(
+        private string $id,
+        private string $type,
+        private float $amount
+    ) {
+    }
+}
